6659993515989b3511bcb54ded78bbea2e8f4418
twyair <yairt16@gmail.com>
2022-10-17 12:49:13 +0300

    create package

---
+++ hebrew_transliterate/__init__.py
@@ -0,0 +1,9 @@
+from ._heb_to_trans import convert as heb_to_trans
+from ._trans_to_heb import convert as trans_to_heb
+from ._trans_to_ktiv_male import convert as trans_to_ktiv_male
+
+__all__ = [
+    "heb_to_trans",
+    "trans_to_heb",
+    "trans_to_ktiv_male",
+]
